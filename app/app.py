407b5b1d96e66fb2eef35d488a06214873a12207
Akshay Kadidal <akshaykadidal@gmail.com>
2024-12-26 23:15:43 +0000

    test mulitpage

---
+++ app/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/features')
+def features():
+    return render_template('features.html')
+
+@app.route('/products')
+def products():
+    return render_template('product.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
